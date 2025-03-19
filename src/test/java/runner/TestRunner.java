da4069273e445171212f7821e7e098e3e7232f79
HarikaBC <139906009+HarikaBC@users.noreply.github.com>
2025-03-19 09:14:13 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "stepDefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports.html"},
+    plugin = {"pretty","json:target/cucumber-reports-json/Cucumber.json"},
     monochrome = true
 )
 
